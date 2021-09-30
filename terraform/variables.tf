c49fb8f7dde5a6101a8acb1c18a8835b69be22a4
testuser681 <piotrdrabicki99@gmail.com>
2021-09-30 10:20:10 +0200

    iam role for eks

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+variable "default_region" {
+    default = "us-east-2"
+}
+
+variable "default_az" {
+    default = "us-east-2a"
+}
+variable "default_vpc_id" {
+    default = "vpc-d8ef86b3"
+}
+
+variable "default_instance" {
+    default = "t2.micro"
+}
+
+variable "default_key" {
+    default = "My_key"
+}
