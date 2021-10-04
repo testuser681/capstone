0f63afe1e649368f405e1c25cf2e0dd1f24df611
testuser681 <piotrdrabicki99@gmail.com>
2021-10-04 14:27:31 +0200

    jenkins not working yet

---
+++ terraform/jenkins.tf
@@ -0,0 +1,11 @@
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "jenkins"
+  version    = "8.0.14"
+  namespace  = "jenkins"
+  timeout    = 600
+  values = [
+    "${file("./jenkins/helm-values.yaml")}"
+  ]
+}
