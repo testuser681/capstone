70e9aecc3be156170a589edbfebff940dca9d5a1
testuser681 <piotrdrabicki99@gmail.com>
2021-10-05 13:08:59 +0200

    connection error fixed

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,7 +1,11 @@
-provider "helm" {
-  kubernetes {
-    config_path    = "~/.kube/config"
-    config_context = join("", ["arn:aws:eks:", var.region, ":", var.user_id, ":cluster/", local.cluster_name])
-  }
+data "aws_eks_cluster_auth" "cluster-auth" {
+  name = data.aws_eks_cluster.cluster.name
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster-auth.token
+  }
+}
